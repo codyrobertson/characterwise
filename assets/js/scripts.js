223046e111471f7de9d8f34bdccb3cbfcac85588
codyrobertson <digitalcody@gmail.com>
2016-07-20 14:58:51 -0700

    This is a pain in my ass—took out the fixed nav

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,16 +25,11 @@ jQuery(document).ready(function() {
 
 $('.top-content .text').waypoint(function(direction) {
   if (direction === 'down') {
-		$('nav').removeClass('navbar-no-bg');
-		$(".navbar-inverse ul.navbar-nav li a").css("color", "black");
-		$("#logo").attr("src","assets/img/Logo_Long/Regular/longreg.png");
+$("#logo").attr("src","assets/img/Logo_Long/Regular/longreg.png");
 		$('#buy').css("background-color", "#4dbea7");
 		$('#buy').css("border-bottom", "2px #409d8a solid");
   }
   else {
-		$('nav').addClass('navbar-no-bg');
-		$("#logo").attr("src","assets/img/Logo_Long/White/longW.png");
-		$(".navbar-inverse ul.navbar-nav li a").css("color", "white");
 		$('#buy').css("background-color", "RGB(249, 157, 30)");
 		$('#buy').css("border-bottom", "2px #b67316 solid");
   }
